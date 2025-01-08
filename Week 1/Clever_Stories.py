a32ad8176c6f1127bbeb76ee592339ac793a0e9f
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-08 20:20:20 -0300

    Mad libs 1st version - added variables

---
+++ Week 1/Clever_Stories.py
@@ -0,0 +1,12 @@
+import random
+
+print("Please enter the following: ")
+
+adjective = input("Adjective: ")
+animal = input("Animal: ")
+verb = input("Verb: ")
+exclamaation = input("Exclmation: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+
+print("Your Story is:\n")

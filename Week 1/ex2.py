deb79e33ec39a79ea40821e591b1c8ff1e1743aa
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-08 01:21:20 -0300

    changed_made_on_the_string_suffix

--- Week 1/ex2.py
+++ Week 1/ex2.py
@@ -3,10 +3,18 @@ Author: Lucas Miranda
 Purpose: Ask the user their name and print on the screnn
 """
 
+#First part of the activity
 name1 = input("What is your frist name? ")
-name1 = name1.titlte()
+name1 = name1.capitalize()
 
 name2 = input("What is your last name? ")
-name2 = name2.titlte()
+name2 = name2.capitalize()
 
 print(f"Your name is {name2}, {name1} {name2}.")
+
+#Second part of the activity
+name1 = input("What is your frist name? ")
+
+name2 = input("What is your last name? ")
+
+print(f"Your name is {name2.title()}, {name1.title()} {name2.title()}.")

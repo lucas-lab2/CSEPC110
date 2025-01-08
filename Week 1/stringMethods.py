337cf0a5674e1db044dd6d398d36fb477a7fecaa
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-08 01:02:50 -0300

    strings_methos3

--- Week 1/stringMethods.py
+++ Week 1/stringMethods.py
@@ -1,3 +1,4 @@
+"""
 #Will modify the input so that it will convert the information from the input to a lowecase state
 
 email = input("What is your email address? ")
@@ -9,3 +10,9 @@ print(email)
 #change permantely the variable to a lowercase state
 email = email.lower()
 print(email)
+"""
+
+book = input ("What is the title of your favorite book? ")
+print(book.capitalize()) #The first letter will be in uppercase
+print(book.upper())#all the letters will be in uppercase
+print(book.title())#Only the first letter of each word will be in uppercase

60d090bb2711fd7181ca39685577b78bbdf2ccd7
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-08 01:09:41 -0300

    ex2

---
+++ Week 1/ex2.py
@@ -0,0 +1,12 @@
+"""
+Author: Lucas Miranda
+Purpose: Ask the user their name and print on the screnn
+"""
+
+name1 = input("What is your frist name? ")
+name1 = name1.capitalize()
+
+name2 = input("What is your last name? ")
+name2 = name2.capitalize()
+
+print(f"Your name is {name2}, {name1} {name2}")

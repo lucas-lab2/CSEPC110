5db1ee4f6ee063a8bf5b5a0626881a6a66f50e95
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-08 20:38:03 -0300

    Bugs fixed and first version completed

--- Week 1/Clever_Stories.py
+++ Week 1/Clever_Stories.py
@@ -1,12 +1,15 @@
-import random
-
 print("Please enter the following: ")
 
 adjective = input("Adjective: ")
 animal = input("Animal: ")
 verb = input("Verb: ")
-exclamaation = input("Exclmation: ")
+exclamation = input("Exclmation: ")
 verb2 = input("Verb: ")
 verb3 = input("Verb: ")
 
 print("Your Story is:\n")
+print(f"""The other day, I was really in trouble. It all started when I saw a very
+{adjective.lower()} {animal.lower()} {verb.lower()} down the hallway. \"{exclamation.capitalize()}!\" I yelled. But all
+I could think to do was to {verb2.lower()} over and over. Miraculously,
+that caused it to stop, but not before it tried to {verb3.lower()}
+right in front of my family.""")

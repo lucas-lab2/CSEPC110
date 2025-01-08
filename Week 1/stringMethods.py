7f24f8e39b14f8f4a9b65c3bdfadcb62c11c190c
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-08 00:55:27 -0300

    string_variables

---
+++ Week 1/stringMethods.py
@@ -0,0 +1,7 @@
+#Will modify the input so that it will convert the information from the input to a lowecase state
+
+email = input("What is your email address? ")
+print(email.lower())
+
+#It won't change the value of the input
+print(email)

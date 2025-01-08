f014500b811d04c41dafe9e7199662a524dbea7a
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-08 01:12:56 -0300

    change_string

--- Week 1/ex2.py
+++ Week 1/ex2.py
@@ -4,9 +4,9 @@ Purpose: Ask the user their name and print on the screnn
 """
 
 name1 = input("What is your frist name? ")
-name1 = name1.capitalize()
+name1 = name1.titlte()
 
 name2 = input("What is your last name? ")
-name2 = name2.capitalize()
+name2 = name2.titlte()
 
 print(f"Your name is {name2}, {name1} {name2}.")

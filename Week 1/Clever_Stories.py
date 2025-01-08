7c6ebbe0c3e710ea7348a1b305b45ea77f62ecff
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-08 20:52:45 -0300

    version 2 in production - loop added

--- Week 1/Clever_Stories.py
+++ Week 1/Clever_Stories.py
@@ -1,3 +1,5 @@
+import random
+
 print("Please enter the following: ")
 
 adjective = input("Adjective: ")
@@ -13,3 +15,16 @@ print(f"""The other day, I was really in trouble. It all started when I saw a ve
 I could think to do was to {verb2.lower()} over and over. Miraculously,
 that caused it to stop, but not before it tried to {verb3.lower()}
 right in front of my family.""")
+
+while True:
+    stop = input("\nDo you want to make ir funnier?: Yes - 1 No- 0: ")
+    stop2 = int(stop)
+    if stop2 == 0:
+        break
+    else:
+        print("Please enter 3 adjectives: ")
+        adjectives = [input("Enter an adjective: ") for _ in range(3)]
+
+        print("Please enter 3 adjectives: ")
+        adjectives = [input("Enter an adjective: ") for _ in range(3)]
+    

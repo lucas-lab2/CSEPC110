ad0feec4cb336fd1b7684be021ee6fa0d0287be4
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-14 23:49:39 -0300

    added the author header

--- Week 1/temperature_converter.py
+++ Week 1/temperature_converter.py
@@ -1,3 +1,8 @@
+"""
+Author: Lucas Miranda
+
+Purpose: Convert degrees in Fahrenheit to Celsius.
+"""
 fahrenheit = float(input("What is the temperature in Fahrenheit? "))
 celsius = float((fahrenheit - 32)/1.8)
 print(f"The temperature in Celsius is: {celsius: .1f} degrees.")

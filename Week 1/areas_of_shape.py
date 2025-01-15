b19cc56fc28bf1939b44ae6610bc3beadb11c044
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-15 00:31:20 -0300

    added the rectangulo calculus

--- Week 1/areas_of_shape.py
+++ Week 1/areas_of_shape.py
@@ -5,13 +5,22 @@ Purpose: Areas of Shape
 
 import math
 
-stop = 1
-
 while True:
     square = float(input("What is the length of a side of the square? "))
     square2 = square * square 
     print(f"The area of the square is: {square2: .1f}") 
- 
+    
+    rectangle1 = float(input("What is the length of rectangle? "))
+    rectangle2 = float(input("What is the width of rectangle? "))
+    rectangle3 = rectangle1 * rectangle2
+    print(f"The area of the rectangle is: {rectangle3: .1f}")
+    
+    
+    radius = float(input("What is the length of rectangle? "))
+    rectangle2 = float(input("What is the width of rectangle? "))
+    rectangle3 = rectangle1 * rectangle2
+    print(f"The area of the rectangle is: {rectangle3: .1f}")
+    
     while True:
         cont = int(input("Do you want to continue? 1 - Yes 0 - No: "))
         if cont == 1 or cont == 0:

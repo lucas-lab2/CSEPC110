d5003334404f902699abc47d0935cf8891a9f3ce
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-15 00:44:34 -0300

    added the strech 1 and 2

--- Week 1/areas_of_shape.py
+++ Week 1/areas_of_shape.py
@@ -7,24 +7,33 @@ import math
 
 while True:
     square = float(input("What is the length of a side of the square? "))
-    square2 = square * square 
-    print(f"The area of the square is: {square2: .1f}") 
+    area = square * square 
+    print(f"The area of the square is: {area: .1f}") 
     
-    rectangle1 = float(input("What is the length of rectangle? "))
-    rectangle2 = float(input("What is the width of rectangle? "))
-    rectangle3 = rectangle1 * rectangle2
-    print(f"The area of the rectangle is: {rectangle3: .1f}")
+    length = float(input("What is the length of rectangle? "))
+    width = float(input("What is the width of rectangle? "))
+    area = length * width
+    print(f"The area of the rectangle is: {area: .1f}")
     
     
-    radius = float(input("What is the length of rectangle? "))
-    rectangle2 = float(input("What is the width of rectangle? "))
-    rectangle3 = rectangle1 * rectangle2
-    print(f"The area of the rectangle is: {rectangle3: .1f}")
+    radius = float(input("What is the radius of the circle? "))
+    area_circle = math.pi * radius**2
+    print(f"The area of the circle is: {area_circle: .1f}")
+    
+    value = float(input("What is the value to be used? "))
+    
+    # Square calculus
+    area_square = value * value
+    area_circle = math.pi * (value ** 2)
+    valume_cube = value ** 3
+    volume_sphere = (4/3) * (math.pi * (value ** 3))
+    
+    
     
     while True:
         cont = int(input("Do you want to continue? 1 - Yes 0 - No: "))
         if cont == 1 or cont == 0:
-            break  # Sai do loop se a entrada for válida
+            break  # leave loop
         print("Invalid input. Please enter 1 or 0.")
         
     if cont == 0:

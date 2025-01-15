36447944053ce700d97b4decf2853368de0355f7
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-14 23:48:48 -0300

    final version

--- Week 1/temperature_converter.py
+++ Week 1/temperature_converter.py
@@ -1,3 +1,3 @@
 fahrenheit = float(input("What is the temperature in Fahrenheit? "))
-celsius = (fahrenheit -32)/1.8
-print(f"The temperature in Celsius is {celsius: 2.f} degrees.")
+celsius = float((fahrenheit - 32)/1.8)
+print(f"The temperature in Celsius is: {celsius: .1f} degrees.")

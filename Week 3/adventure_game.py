137099b29627ab6e602f5d4488de6871b425c9a7
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-18 12:14:19 -0300

    adding the first conditions

--- Week 3/adventure_game.py
+++ Week 3/adventure_game.py
@@ -2,12 +2,23 @@
 Author: Lucas Miranda
 Purpose: Adventure game
 """
+import textwrap
 
 while True:
+    text = "You are a faithful follower of the prophet Nephi, tasked with retrieving sacred records hidden deep within the wilderness. The journey will test your faith, courage, and wisdom. As you stand at the edge of the camp, you face three paths leading into the wilderness. Which path will you take?"
+    line = textwrap.fill(text, width = 50)
+    print(line)
+    
+    choices = input("North\nEast\nWest\n")
+    choices1 = choices.lower()
+    choices2 = choices1 != "north" or choices1 != "east" or choices1 != "west"
+    
+    if choices2:
+        print("Please, choose a valid option! ")
     
     
     while True:
-        cont = int(input("Do you want to convert another grade? 1 - Yes 0 - No: "))
+        cont = int(input("Do you want to try a different scenario? 1 - Yes 0 - No: "))
         if cont == 1 or cont == 0:
             break  # leave loop
         print("Invalid input. Please enter 1 or 0.")

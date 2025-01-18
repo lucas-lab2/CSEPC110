7ebf34f890131d71ac7d6fce86239222b1d7e062
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-18 15:28:36 -0300

    added comments about the creativity points added

--- Week 3/adventure_game.py
+++ Week 3/adventure_game.py
@@ -2,6 +2,7 @@
 Author: Lucas Miranda
 Purpose: Adventure game
 """
+#Showing creativity: added loops, entry validation, reward counting added, use of lists
 import textwrap
 
 # Initialize the reward counters

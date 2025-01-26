735d25a3d5ade1b551d3a899c9ec82e36aad9189
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-25 21:44:21 -0300

    fixation activity

---
+++ Week 4/fixation_activity.py
@@ -0,0 +1,17 @@
+#Given the following list of items (copy and paste this line into your program):
+colors = ["red", "blue", "green", "yellow"]
+
+for i in colors:
+    print(i)
+
+print("\n")
+
+for i in range (1, 9):
+    print(i)
+
+print("\n")
+
+for i in range (2, 21, 2):
+    print(i)
+
+

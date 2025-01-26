2adec66b36754ae86cbcdfd621d5a10ec4b91461
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-25 21:35:17 -0300

    studies about for looping

---
+++ Week 4/for_looping_notes.py
@@ -0,0 +1,12 @@
+numbers = [1,2,3,4,5]
+
+for number in numbers:
+    print(number)
+
+
+for number in range(1, 100): #I'm defining a ranger from 1 to 11 rather than a list of numbers
+    print(number)
+
+
+for number in range(1, 100, 2): #I'm defining a ranger from 1 to 100 with a step of 2
+    print(number)

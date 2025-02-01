3612d55acf0f94d9905d6ebcdab7a706260cd6c1
Lucas Felipe <lucas.miranda880@outlook.com>
2025-01-31 22:04:05 -0300

    fixed issues with hangman game

--- Week 4/hangman_game.py
+++ Week 4/hangman_game.py
@@ -1,45 +1,34 @@
 import random
 
 words = ["Moroni", "Lehi", "Mormon", "Prophet", "Scripture", "Liahona", "Zarahemla", "Jaredite", "Nephite"]
-word = random.choice(words)
-letter_guessed = []
+secret_word = random.choice(words).lower()
 chances = 4
 won = False
 
-while True:
-    for letter in word:
-        if letter.lower() in letter_guessed:
-            print(letter, end=" ")
-        else:
-            print("_", end=" ")
-    print(f"\nChances left: {chances}") 
-    attempt = input("Guess the word: ").lower()
+print("Your hint is: " + "_ " * len(secret_word))
 
-    if len(attempt) == 1:
-        if attempt not in letter_guessed:
-            letter_guessed.append(attempt)
-        if attempt not in word:
-            chances -= 1
-    elif attempt == word:
+while chances > 0 and not won:
+    guess = input("Guess the word: ").lower()
+    
+    if len(guess) != len(secret_word):
+        print(f"Please enter a {len(secret_word)}-letter word.")
+        continue
+    
+    if guess == secret_word:
         won = True
-        break
     else:
-        for letter in attempt:
-            if letter not in letter_guessed:
-                letter_guessed.append(letter)
-        if attempt != word:
-            chances -= 1
+        hint = []
+        for i in range(len(secret_word)):
+            if guess[i] in secret_word:
+                hint.append(guess[i])
+            else:
+                hint.append('_')
+        print("Your hint is:", " ".join(hint))
+        chances -= 1
+        print(f"Chances left: {chances}")
 
-    won = True
-    for letter in word:
-        if letter.lower() not in letter_guessed:
-            won = False
-        
-    if chances == 0 or won:
-        break
-     
 if won:
     print("\nYou won!")
 else:
     print("\nYou lost!")
-    print("\nThe word was:", word)
+    print(f"The word was: {secret_word}")

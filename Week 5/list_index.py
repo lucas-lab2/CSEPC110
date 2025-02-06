720af653c90abf51ee14e75b8f10ace9695e6bb7
Lucas Felipe <lucas.miranda880@outlook.com>
2025-02-06 01:27:56 -0300

    list_index

---
+++ Week 5/list_index.py
@@ -0,0 +1,9 @@
+#index =     0      1       2         3         4
+colors = ['red', 'blue', 'green', 'yellow', 'black']
+
+colors.pop(2) # remove an item from the list
+colors.insert(2, 'purple') # insert an item at a specific index
+for i in range(len(colors)):
+    color = colors[i]
+    human_index = i + 1 # human index starts at 1
+    print(f" {human_index} - {color}") # red, blue, green, yellow, black 

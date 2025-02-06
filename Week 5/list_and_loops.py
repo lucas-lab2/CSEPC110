be38b3534c4e7445b334dfbcc3e54d56cbefd2ad
Lucas Felipe <lucas.miranda880@outlook.com>
2025-02-06 00:55:13 -0300

    list and loops

---
+++ Week 5/list_and_loops.py
@@ -0,0 +1,27 @@
+"""
+clients = []
+new_client = ""
+
+while new_client != "exit":
+    new_client = input("Enter the new client: ")
+    if new_client != "exit":
+        clients.append(new_client)
+
+    print("The clients are: ")
+    for client in clients:
+        print(f" {client}")
+"""
+
+points_scored = []
+new_points = 0
+total = 0
+
+while new_points != "exit":
+    new_points = input("Enter the points scored: ")
+    if new_points != "exit":
+        points_scored.append(new_points)
+
+    print("The points scored are: ")
+    for points in points_scored:
+        total += points
+        print(f" {total}")

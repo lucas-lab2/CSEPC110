11ede3326c58a16c597b4f6213f5d36e02a4f39f
Lucas Felipe <lucas.miranda880@outlook.com>
2025-02-08 01:58:48 -0300

    first simple program that retrieves the dates and the total of visits of a website

--- Week 6/file_data_practice.py
+++ Week 6/file_data_practice.py
@@ -1,6 +1,13 @@
 import os
 
-file_path = os.path.join(os.path.dirname(__file__), "demo.txt")
+file_path = os.path.join(os.path.dirname(__file__), "traffic.csv")
 
-with open(file_path) as demo_file:
+with open(file_path) as web_traffic:
+    for line in web_traffic:
+        line_stripped = line.strip()
+        line_split = line_stripped.split(",")
 
+        date = line_split[0]
+        visits = line_split[1]
+
+        print(f"The date the data was accessed was on: {date}, and the total of visits were: {visits}")

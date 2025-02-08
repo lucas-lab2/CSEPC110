26394e59d92e86701ee13cadee7dd09f07282958
Lucas Felipe <lucas.miranda880@outlook.com>
2025-02-08 02:04:07 -0300

    bugs fixed

--- Week 6/file_data_practice.py
+++ Week 6/file_data_practice.py
@@ -1,5 +1,5 @@
 import os
-
+visits = 0
 file_path = os.path.join(os.path.dirname(__file__), "traffic.csv")
 
 with open(file_path) as web_traffic:
@@ -9,5 +9,5 @@ with open(file_path) as web_traffic:
 
         date = line_split[0]
         visits = line_split[1]
-
         print(f"The date the data was accessed was on: {date}, and the total of visits were: {visits}")
+    print(f"The total number of visits are: {visits}")

b2164e6bb5f0c3d7501c568cf273ab703fbc0022
Lucas Felipe <lucas.miranda880@outlook.com>
2025-02-08 01:34:46 -0300

    learning how to split strings

--- Week 6/file_demo.py
+++ Week 6/file_demo.py
@@ -1,8 +1,23 @@
 import os
-file_path = os.path.join(os.path.dirname(__file__), "courses.txt")
-#courses_file = open(file_path)
+# file_path = os.path.join(os.path.dirname(__file__), "courses.txt")
+# #courses_file = open(file_path)
 
-with open(file_path) as courses_file:
-    for line in courses_file:
-        print(line)
+# with open(file_path) as courses_file: #closes the file automatically after the program runs
+#     for line in courses_file:
+#         print(line)
 
+#Learning how to remove remove whitespaces 
+
+name = "   John Doe   "
+name_stripped = name.strip() #removes whitespaces from the beginning and end of the string
+
+print(name) #prints "   John Doe   "
+print(f"***{name_stripped}***") #prints John Doe
+
+colors = "red green blue orange yellow"
+
+color_list = colors.split() #splits the string into a list of strings
+
+for color in color_list:
+    color_stripped = color.strip()
+    print(color_stripped, end=' ')

0a53ba34e6a22a6b209155c044457c748d514936
Lucas Felipe <lucas.miranda880@outlook.com>
2025-02-22 16:12:19 -0300

    learned how to pass data to functions and get a result

---
+++ Week 7/passing_data_to_funcitons.py
@@ -0,0 +1,10 @@
+def add (first, second):
+    """Add two numbers together and return the result"""
+    result = first + second
+    return result
+x = 3
+y = 5
+
+total = add(x, y)
+print(total)
+# The function add takes two arguments, first and second, and returns the result of adding the two arguments together. The function is called with the arguments x and y, which are assigned the values 3 and 5, respectively. The function returns the result of adding 3 and 5, which is 8. The result is stored in the variable total, which is then printed to the console. The output of the program is 8.
